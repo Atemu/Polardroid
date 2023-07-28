b6beeac3e2369d34b2806ee5821f71af97cac5d1
Atemu <atemu.main@gmail.com>
2023-07-28 19:16:52 +0200

    exclusions: update

--- exclusions.nix
+++ exclusions.nix
@@ -1,15 +1,26 @@
 [
   "data/ru.meefik.linuxdeploy/"
   "data/*/app_tmpdir/mozilla-temp-*"
-  "data/*/cache/"
+  "**/cache/"
   "data/com.google.android.apps.docs/"
   "data/com.spotify.music/files/spotifycache/Storage/"
   "data/com.termux.nix/files/"
   "data/com.termux/files/"
-  "data/org.fdroid.fdroid/files/*.apk"
+  "**/org.fdroid.fdroid/files/*.apk" # /data/data/... as well as /data/media/0/Android/...
+  "data/com.aurora.store/files/Downloads/"
+  "data/air.de.fahren_lernen.app/de.fahren-lernen.app/Local Store/"
+  "data/app.organicmaps/files/*/*.mwm" # Replacable map tiles
+  "data/com.google.android.apps.docs.editors.*" # All online anyways
+  "data/com.simplemobiletools.gallery.pro/files/storage/"
   "media/0/0/"
-  "media/0/Android/data/com.spotify.music/files/spotifycache/Storage/"
+  "media/0/easy xkcd/"
+  "media/0/Android/data/com.spotify.music/files/spotifycache/"
+  "media/0/Android/data/com.generalmagic.magicearth/files/Download/MAGICEARTH/Data/Temporary/" # Replacable map tiles
+  "media/0/Android/data/net.osmand.plus/files/*.obf" # Replacable map tiles
+  "media/0/Android/data/net.osmand.plus/files/*/*.obf" # Replacable map tiles
+  "media/0/Android/data/org.fitchfamily.android.gsmlocation" # Just a local snapshot of an online DB
   "media/0/DCIM/.thumbnails/"
   "media/0/Movies/"
+  "media/0/Kiwix/"
   "system/package_cache/1/"
 ]
