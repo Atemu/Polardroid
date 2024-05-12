4d59c63f968e73c9f3709e5f3a72fdfb1591c921
Atemu <atemu.main@gmail.com>
2024-05-12 14:41:39 +0200

    host: format cmds

--- modules/host/cmds.nix
+++ modules/host/cmds.nix
@@ -19,16 +19,17 @@ let
   # TODO make non-tmpfs installation work again
   useTmpfs = true;
 
-  sshdConfig = (import <nixpkgs/nixos> {
-    configuration.services.openssh = {
-      enable = true;
-      settings = {
-        PermitRootLogin = "no";
-        PasswordAuthentication = false;
+  sshdConfig =
+    (import <nixpkgs/nixos> {
+      configuration.services.openssh = {
+        enable = true;
+        settings = {
+          PermitRootLogin = "no";
+          PasswordAuthentication = false;
+        };
+        hostKeys = [ ];
       };
-      hostKeys = [ ];
-    };
-  }).config.environment.etc."ssh/sshd_config".source;
+    }).config.environment.etc."ssh/sshd_config".source;
   sshdConfigPatched = runCommand "sshdConfigPatched" { } ''
     substitute ${sshdConfig} $out --replace "UsePAM yes" ""
   '';
@@ -74,50 +75,60 @@ rec {
     fi
   '';
 
-  adbScriptBin = name: script: writeShellScriptBin name ''
+  adbScriptBin =
+    name: script:
+    writeShellScriptBin name ''
       PATH=${android-tools}/bin/:$PATH
 
       ${script}
-  '';
+    '';
 
-  installCmd = adbScriptBin "installCmd" (''
-    if adb shell 'ls -d ${prefix} 2>&1 > /dev/null' ; then
-      echo Error: Nix environment has been installed already. Remove it using the removeCmd.
-      exit 1
-    fi
+  installCmd = adbScriptBin "installCmd" (
+    ''
+      if adb shell 'ls -d ${prefix} 2>&1 > /dev/null' ; then
+        echo Error: Nix environment has been installed already. Remove it using the removeCmd.
+        exit 1
+      fi
 
-    # Copy Nix store over to the device
-    adb shell mkdir -p ${prefix}
-  '' + optionalString useTmpfs ''
-    adb shell mount -t tmpfs tmpfs ${prefix}
-  '' + ''
-    nix-store --query --requisites ${recoveryEnv} | cut -c 2- \
-      | tar cf - -C / --files-from=/dev/stdin | ${getExe pv} | gzip -2 | adb shell 'gzip -d | tar xf - -C ${prefix}/'
-    adb shell "mkdir -p ${prefix}/nix/var/nix/profiles/ && ln -s ${recoveryEnv} ${prefix}/nix/var/nix/profiles/default"
+      # Copy Nix store over to the device
+      adb shell mkdir -p ${prefix}
+    ''
+    + optionalString useTmpfs ''
+      adb shell mount -t tmpfs tmpfs ${prefix}
+    ''
+    + ''
+      nix-store --query --requisites ${recoveryEnv} | cut -c 2- \
+        | tar cf - -C / --files-from=/dev/stdin | ${getExe pv} | gzip -2 | adb shell 'gzip -d | tar xf - -C ${prefix}/'
+      adb shell "mkdir -p ${prefix}/nix/var/nix/profiles/ && ln -s ${recoveryEnv} ${prefix}/nix/var/nix/profiles/default"
 
-    # Provide handy script to enter an env with Nix
-    adb push ${enterScript} ${prefix}/enter
-    adb push ${cleanupScript} ${prefix}/cleanup
-    adb push ${removalScript} ${prefix}/remove
-    adb shell chmod +x ${prefix}/enter
-    adb shell chmod +x ${prefix}/cleanup
-    adb shell chmod +x ${prefix}/remove
-    echo 'Nix has been installed, you can now run `adb shell` and then `${prefix}/enter` to get a Nix environment'
+      # Provide handy script to enter an env with Nix
+      adb push ${enterScript} ${prefix}/enter
+      adb push ${cleanupScript} ${prefix}/cleanup
+      adb push ${removalScript} ${prefix}/remove
+      adb shell chmod +x ${prefix}/enter
+      adb shell chmod +x ${prefix}/cleanup
+      adb shell chmod +x ${prefix}/remove
+      echo 'Nix has been installed, you can now run `adb shell` and then `${prefix}/enter` to get a Nix environment'
 
-    # Fake `/etc/passwd` to make SSH work
-    adb shell 'mkdir -p ${prefix}/etc/'
-    adb shell 'echo "root:x:0:0::/:" > ${prefix}/etc/passwd'
-  '');
+      # Fake `/etc/passwd` to make SSH work
+      adb shell 'mkdir -p ${prefix}/etc/'
+      adb shell 'echo "root:x:0:0::/:" > ${prefix}/etc/passwd'
+    ''
+  );
 
-  removeCmd = adbScriptBin "removeCmd" (''
-    adb shell sh ${prefix}/remove
-  '' + optionalString useTmpfs ''
-    adb shell umount ${prefix}
-    adb shell rmdir ${prefix}
-  '' + ''
+  removeCmd = adbScriptBin "removeCmd" (
+    ''
+      adb shell sh ${prefix}/remove
+    ''
+    + optionalString useTmpfs ''
+      adb shell umount ${prefix}
+      adb shell rmdir ${prefix}
+    ''
+    + ''
 
-    echo "All traces of Nix removed."
-  '');
+      echo "All traces of Nix removed."
+    ''
+  );
 
   # One step because you only need to run this once and it works from there on
   setupSsh = writeShellScriptBin "setupSsh" ''
