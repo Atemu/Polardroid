f1278b7f6b1c262bd2e8c38936fc43310a28bbff
Atemu <atemu.main@gmail.com>
2024-05-13 14:11:19 +0200

    default: format

--- default.nix
+++ default.nix
@@ -6,22 +6,20 @@
 
 let
   eval = lib.evalModules {
-  modules =
-    lib.mapAttrsToList (n: v: ./modules + "/${n}/module.nix") (builtins.readDir ./modules)
-    ++ [
-      (
-        { config, ... }:
-        {
-          _module.args = {
-            inherit pkgs;
-            inherit (config.system) targetPkgs crossPkgs;
-          };
-        }
-      )
-      configuration
-    ];
-};
+    modules =
+      lib.mapAttrsToList (n: v: ./modules + "/${n}/module.nix") (builtins.readDir ./modules)
+      ++ [
+        (
+          { config, ... }:
+          {
+            _module.args = {
+              inherit pkgs;
+              inherit (config.system) targetPkgs crossPkgs;
+            };
+          }
+        )
+        configuration
+      ];
+  };
 in
-eval.config.host.env // {
-  passthru = eval;
-}
+eval.config.host.env // { passthru = eval; }
