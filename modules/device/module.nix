17a1701b60f6f23ee2bfdf71e7c7b604c6a5d96c
Atemu <git@atemu.net>
2024-12-20 20:06:16 +0100

    backup: add restore script

--- modules/device/module.nix
+++ modules/device/module.nix
@@ -20,6 +20,10 @@ in
       internal = true;
       type = lib.types.package;
     };
+    polardroid-restore = mkOption {
+      internal = true;
+      type = lib.types.package;
+    };
     packages = mkOption {
       description = ''
         Packages to install inside the device env.
@@ -57,6 +61,7 @@ in
           lib.optionals config.backup.enable [
             this.polardroid-borg
             this.polardroid-ncdu
+            this.polardroid-restore
           ]
           ++ this.packages;
       };
